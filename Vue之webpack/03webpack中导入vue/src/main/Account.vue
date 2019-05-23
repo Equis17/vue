99d5ecfb3b2edb3923f26afb18e4816722b5dac7
equis <q1158582010@qq.com>
2019-05-23 16:53:52 +0800

    vue中使用webpack

---
+++ Vue之webpack/03webpack中导入vue/src/main/Account.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <h3>Account</h3>
+    <router-link to="/account/login">login</router-link>
+    <router-link to="/account/register">register</router-link>
+    <router-view></router-view>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "Account"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue之webpack/03webpack中导入vue/src/main/GoodsList.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h3>
+        GoodsList
+    </h3>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "GoodsList"
+    }
+</script>
+
+<style scoped>
+
+</style>
