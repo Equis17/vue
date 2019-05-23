35cdb2209cfc4b95aefbe3326f389945ab56b17f
equis <q1158582010@qq.com>
2019-05-23 14:36:30 +0800

    vue中使用webpack

---
+++ Vue之webpack/03webpack中导入vue/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: path.join(__dirname, 'src/js/main.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [ // 添加plugins节点配置插件
+        new htmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),//模板路径
+            filename: 'index.html'//自动生成的HTML文件的名称
+        })
+
+    ],
+    module: {
+        rules: [
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            {test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']},
+            {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']},
+            {test: /\.(jpg|png|gif|bmp|jpeg)$/, use: ['url-loader']},
+            {test: /\.(ttf|eot|svg|woff|woff2)$/, use: ['url-loader']},
+            {test: /\.js$/, use: ['babel-loader'], exclude: /node_modules/}
+        ]
+    }
+};
